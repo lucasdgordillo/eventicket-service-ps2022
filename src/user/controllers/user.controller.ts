48e5d56a2167669e23b13b1dfb2b382d9c5384fc
Lucas Gordillo <lucas@Lucas-MacBook-Pro.local>
2022-06-30 18:04:32 -0300

    Agregar endpoint para editar y ver datos de un usuario

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpException, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Param, Put, Request, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { User } from '../models/user.interface';
 import { UserService } from '../services/user.service';
 
 @Controller('users')
@@ -9,7 +10,7 @@ export class UserController {
   ) {}
 
   @UseGuards(JwtGuard)
-  @Get()
+  @Get('/by-creator-id')
   async getUsersByCreatorId(@Request() req) {
     return this.usersService.getAllUsersByCreatorId(req.user).then(async (users) => {
       return { data: users };
@@ -23,6 +24,26 @@ export class UserController {
   async getProductorUsers() {
     return this.usersService.getAllProductorUsers().then(async (users) => {
       return { data: users };
+    }).catch(e => {
+      throw new HttpException(e.response, e.status);
+    });
+  }
+
+  @UseGuards(JwtGuard)
+  @Put(':id')
+  async updateUserInformation(@Param('id') id: number, @Body() newData: User) {
+    return this.usersService.update(id, newData).then(async () => {
+      return { message: 'Update Success' };
+    }).catch(e => {
+      throw new HttpException(e.response, e.status);
+    });
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('/user-data')
+  async getUserInfo(@Request() req) {
+    return this.usersService.getUserInformation(req.user).then(async (userInfo) => {
+      return { data: userInfo };
     }).catch(e => {
       throw new HttpException(e.response, e.status);
     });
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -12,11 +12,19 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>
   ) {}
 
+  async getUserInformation(user: User) {
+    return await this.userRepository.findOne({ relations: { province: true }, where: { id: user.id }});
+  }
+
   async getAllUsersByCreatorId(user: User) {
     return await this.userRepository.find({ relations: { province: true }, where: { createdBy: user }});
   }
 
   async getAllProductorUsers() {
     return await this.userRepository.find({ where: { role: Role.PRODUCTOR }});
+  }
+
+  async update(id: number, newData: User) {
+    return await this.userRepository.update(id, newData);
   }
 }
